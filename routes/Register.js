b4b3833a9930020b5cd6482fe1814463a5292351
amitabha saha <sagar@indusnet.co.in>
2015-04-01 12:24:26 +0530

    register modified with validations

--- routes/Register.js
+++ routes/Register.js
@@ -1,4 +1,5 @@
 var emailvalidator = require("email-validator");
+var async = require('async');
 var UsersCollection = require('./DbCollections.js').UsersCollection;
 
 exports.Test = function(req,res)
@@ -6,50 +7,112 @@ exports.Test = function(req,res)
     res.send('Test the api is ok...response code 200');
 }
 
-exports.Register = function (req, res) {
+exports.Register = function (req, res) 
+{
     
     var userobj = req.body;
-    
-    //console.log('Adding wine: ' + JSON.stringify(userobj));
-    
+
+    var StatusCode = 200;
+
     //validations : 
+    //201 : invalid email
+    //202 : invalid phone no
+    //203 : user already exists
     if(userobj.profiletype === 'manual')
     {
-        console.log(emailvalidator.validate(userobj.email));
-        if(!emailvalidator.validate(userobj.email))
+        //console.log('isNaN' + isNaN(userobj.email));
+
+        if(isNaN(userobj.email)) //check if it is not a number
         {
-            //res.setHeader(name, value)
-            res.send({"_id" : "" , "StatusCode" : "201"});
-            return;
+            if(!emailvalidator.validate(userobj.email))
+            {
+                StatusCode = "201";
+            }
         }
-        // if(userobj.gender != 'male' || userobj.gender != 'female')
-        // {
-        //     res.send({"_id" : "" , "StatusCode" : "202"});
-        //     return;
-        // }
+        else //It is a phono no of 10 digit or not
+        {
+            var phoneno = /^\d{10}$/;  
+            if(!userobj.email.match(phoneno))  
+            {
+                StatusCode = "202";
+            }
+        }
     }
-    
 
+        //check user already exists
+        async.series([
+        
+        function(callback) {
+            if(userobj.profiletype == 'manual')
+            {
+                UsersCollection.find({email: userobj.email}, function(err,obj) 
+                {
+                    if (err) { StatusCode = 500; }
+                    if(obj.length != 0)
+                        StatusCode = 203;
+                        //return callback(new Error('user exists.'));
+                    callback();
+                })
+            }
+            else
+            {
+                UsersCollection.find({token: userobj.token}, function(err,obj) 
+                {
+                    if (err) { StatusCode = 500; }
+                    if(obj.length != 0)
+                        StatusCode = 203;
+                        //return callback(new Error('user exists.'));
+                    console.log(StatusCode);
+                    callback();
+                })
+            }
+            
+        }, //1st function end
 
-    var User = new UsersCollection({
-        "email": userobj.email,
-    	"gender": userobj.gender,
-    	"age": userobj.age,
-    	"profiletype": userobj.profiletype,
-    	"token" : userobj.token
-    });
-    
-    User.save(function (err, obj) {
-        if (err) 
-        {
-            console.log(err);// ...
-            res.send({"_id" : "" , "StatusCode" : "500"});
-        }
+        ], //end of function serize
+            function(err) { //This function gets called after the two tasks have called their "task callbacks"
 
-        var responseObj = {"_id" : obj._id , "StatusCode" : "200"};
-        res.send(responseObj);
-    });
-}
+            if(StatusCode == 201)
+            {
+                res.send({"_id" : "", "StatusCode" : StatusCode ,"Message" : "Invalid email"});
+            }
+            else if(StatusCode == 202)
+            {
+                res.send({"_id" : "" , "StatusCode" : StatusCode ,"Message" : "Invalid phone no"});
+            }
+            else if(StatusCode == 203)
+            {
+                res.send({"_id" : "", "StatusCode" : StatusCode , "Message" : "User already exists"});
+            }
+            else if(StatusCode == 500)
+            {
+                res.send({"_id" : "", "StatusCode" : StatusCode, "Message" : "User already exists"});
+            }
+            else
+            {
+                var User = new UsersCollection({
+                "email": userobj.email,
+                "gender": userobj.gender,
+                "age": userobj.age,
+                "profiletype": userobj.profiletype,
+                "token" : userobj.token
+                });
+            
+                User.save(function (err, obj) {
+                    if (err) 
+                    {
+                        console.log(err);// ...
+                        res.send({"_id" : "" , "StatusCode" : "500",  "Message" : "Internal server error"});
+                    }
+                    else
+                    {
+                        var responseObj = {"_id" : obj._id , "StatusCode" : "200", "Message" : "OK"};
+                        res.send(responseObj);    
+                    }
+                });
+            }
+        });
+} 
 
 
 exports.SocialLoggin = function (req, res) {
