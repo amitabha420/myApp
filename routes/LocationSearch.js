08cf82edad5fea220695e1020431461a73844a5b
amitabha saha <sagar@indusnet.co.in>
2015-04-08 16:26:04 +0530

    float to string

--- routes/LocationSearch.js
+++ routes/LocationSearch.js
@@ -36,7 +36,10 @@ exports.GeoLocations = function(req,res)
   var limit = parseInt(input.limit);
 	var coordinate = [lat,lng];
 
-	//console.log(coordinate);
+  console.log(parseFloat("5.265"));
+  console.log(lat + "," lng);
+
+	console.log(coordinate);
 
 	adminUsersSchema.aggregate(
                             [{ $match :{'Channel.GeoFencingData.Loc': 
