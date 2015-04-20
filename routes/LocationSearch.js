cfddcf946aa92a8181ea440c3c7eefabe19ac47d
amitabha saha <sagar@indusnet.co.in>
2015-04-20 20:08:12 +0530

    a

--- routes/LocationSearch.js
+++ routes/LocationSearch.js
@@ -48,7 +48,7 @@ exports.GeoLocations = function(req,res)
                                                 $geoNear : 
                                                 {
                                                     $geometry: {"type": "Point", coordinates: coordinate},
-                                                    $maxDistance: 100 / 6378137, 
+                                                    $maxDistance: 10000 / 6378137, 
                                                     distanceMultiplier: 6378137
                                                  }
                                             }
